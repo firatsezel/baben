0c10ad1fec969780e8b632f30380575be4329056
Fırat Sezel <firat.sezel@gmail.com>
2020-05-31 20:05:15 +0300

    initial commit

---
+++ precache-manifest.ad1069c7c3bf39dfbceb98578e839c69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f027523a075e779a6f4fab3036665572",
+    "url": "/baben/index.html"
+  },
+  {
+    "revision": "f24ab031f7add07ffec8",
+    "url": "/baben/static/css/main.cc47c2a8.chunk.css"
+  },
+  {
+    "revision": "de27a532af15d0a5ca67",
+    "url": "/baben/static/js/2.e18e68a3.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/baben/static/js/2.e18e68a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24ab031f7add07ffec8",
+    "url": "/baben/static/js/main.82849c4d.chunk.js"
+  },
+  {
+    "revision": "330d46d2c3833baf15af",
+    "url": "/baben/static/js/runtime-main.72c69f5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/baben/static/media/logo.5d5d9eef.svg"
+  }
+]);
